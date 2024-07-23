4cf09c77c856ff3fec44a210d50ae5e3f8088746
Mario Fragnito <mariofragnitoph@gmail.com>
2024-07-23 13:19:32 +0200

    Adds annoBisestile method

--- src/Es1/Main.java
+++ src/Es1/Main.java
@@ -2,15 +2,29 @@ package Es1;
 
 import java.util.Scanner;
 
+import static java.lang.Integer.parseInt;
+
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Inserisci una stringa");
         String stringa = scanner.nextLine();
-        System.out.println("Il numero di caratteri della stringa è pari? " + PariDispari(stringa));
+        System.out.println("Il numero di caratteri della stringa è pari? " + stringaPariDispari(stringa));
+
+        System.out.println("Inserisci un anno:");
+        int anno = parseInt(scanner.nextLine());
+        System.out.println("L'anno è bisestile? " + annoBisestile(anno));
+
+        scanner.close();
     }
 
-    public static boolean PariDispari(String stringa) {
+    public static boolean stringaPariDispari(String stringa) {
         return stringa.length() % 2 == 0;
+    }
+
+    public static boolean annoBisestile(int anno) {
+        if (anno % 100 == 0) {
+            return anno % 400 == 0 && anno % 4 == 0;
+        } else return anno % 4 == 0;
     }
 }
