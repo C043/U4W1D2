67eb06714bd849909437c112670651859723e864
Mario Fragnito <mariofragnitoph@gmail.com>
2024-07-23 13:25:11 +0200

    Adds stampaNumero method (Es2)

---
+++ src/Es2/Main.java
@@ -0,0 +1,24 @@
+package Es2;
+
+import java.util.Scanner;
+
+import static java.lang.Integer.parseInt;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Inserisci un numero da 0 a 3");
+        int num = parseInt(scanner.nextLine());
+        stampaNumero(num);
+    }
+
+    public static void stampaNumero(int num) {
+        switch (num) {
+            case 0 -> System.out.println("Zero");
+            case 1 -> System.out.println("Uno");
+            case 2 -> System.out.println("Due");
+            case 3 -> System.out.println("Tre");
+            default -> System.out.println("Dovevi inserire un numero da 0 a 3!");
+        }
+    }
+}
